5e6a4659b9b5565a156e049d0ad39818df02d761
Matheus Saldanha Coelho <matheussaldanhacoelho@gmail.com>
2023-03-19 15:45:30 -0300

    chore: implementação concluida

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,15 @@
 <template>
   <main>
-    <div id="chat_container"></div>
+    <div id="chat_container">
+      <div
+        v-for="(chat, i) in wrapper"
+        :key="i"
+        class="wrapper"
+        :class="{ ai: chat.isAi }"
+      >
+        <Chat :chat="chat" :key="i" />
+      </div>
+    </div>
     <form @submit.prevent="fetchAnswer">
       <textarea
         rows="1"
@@ -16,10 +25,45 @@
 </template>
 <script setup>
 import { ref } from "vue";
+import Chat from "../components/Chat.vue";
 
 const question = ref("");
+const wrapper = ref([]);
+const loading = ref(false);
 
-const fetchAnswer = () => {
-  console.log(question.value);
+const fetchAnswer = async () => {
+  try {
+    loading.value = true;
+    wrapper.value.push({
+      isAi: false,
+      value: question.value,
+    });
+    wrapper.value.push({
+      isAi: true,
+      value: "Loading...",
+    });
+    const res = await fetch("http://localhost:8000", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        question: question.value,
+      }),
+    });
+    // console.log(res);
+    const data = await res.json();
+    console.log(data);
+    const parsedData = data.bot.trim();
+    wrapper.value.pop();
+    wrapper.value.push({
+      isAi: true,
+      value: parsedData,
+    });
+  } catch (error) {
+  } finally {
+    loading.value = false;
+    question.value = "";
+  }
 };
 </script>
