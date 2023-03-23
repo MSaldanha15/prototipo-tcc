e2fe59a696b98c64371266b9446b5cff7db9cc8f
Carlos Bionde <bionde.trab@gmail.com>
2023-03-23 18:19:40 -0300

    feat(organizandoPastas); trocando pastas e add icons - Adicionando novos icones - ajustando pastas

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="chat" ref="currentMessage">
     <div class="profile">
-      <img v-if="chat.isAi" src="@/assets/bot.svg" alt="" />
-      <img v-else src="@/assets/user.svg" alt="" />
+      <img v-if="chat.isAi" src="@/assets/svg/bot.svg" alt="" />
+      <img v-else src="@/assets/svg/user.svg" alt="" />
     </div>
     <div class="message">
       {{ chat.value }}
